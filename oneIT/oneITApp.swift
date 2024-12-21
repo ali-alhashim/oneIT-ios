e25d584a0f239e591924d2e758c5254f23d1e356
ali <ali-alhashim@outlook.com>
2024-12-21 12:48:48 +0300

    Initial Commit

---
+++ oneIT/oneITApp.swift
@@ -0,0 +1,17 @@
+//
+//  oneITApp.swift
+//  oneIT
+//
+//  Created by ALI MUSA ALHASHIM on 21-12-2024.
+//
+
+import SwiftUI
+
+@main
+struct oneITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
